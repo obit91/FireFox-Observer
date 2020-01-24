3fce649965bf310d464a5e0b9bf31107c5b71214
obit91 <ohabel@gmail.com>
2020-01-24 20:13:02 +0200

    Created the basic agents view

--- src/Observer.js
+++ src/Observer.js
@@ -1,3 +1,50 @@
+const VIEWS = {
+    main: "main",
+    cookies: 'cookies'
+}
+
+const AGENTS = ['Cookies', 'Trackers']
+
+let tabs = null;
+
+function setHeaders(type) {
+
+    let tab = tabs.pop();
+
+    var activeTabUrl = document.getElementById('header-title');
+
+    let title = 'No Title';
+    if (type == VIEWS.main) {
+        const urlString = tab.url;
+        const url = new URL(urlString);
+        
+        const urlViewArray = urlString.split('/');
+        const currentView = urlViewArray[urlViewArray.length - 1];
+
+        const hostname = url.hostname;
+        title = `Tracking agents in ${hostname} @ ${currentView}`;
+    } else if (type == VIEWS.cookies) {
+        title = "Cookies at: " + tab.title;
+    }
+
+    //set the header of the panel
+    const text = document.createTextNode(title);
+    activeTabUrl.appendChild(text);
+}
+
+function populateAgents() {
+
+    let tab = tabs.pop();
+    const agentsList = document.getElementById('agents-list');
+
+    for (const agent of AGENTS) {
+        let li = document.createElement("li");
+        let content = document.createTextNode(agent);
+        li.appendChild(content);
+        agentsList.appendChild(li);
+      }
+}
+
 function showCookiesForTab(tabs) {
     //get the first tab object in the array
     let tab = tabs.pop();
@@ -36,4 +83,12 @@ function showCookiesForTab(tabs) {
   function getActiveTab() {
     return browser.tabs.query({currentWindow: true, active: true});
   }
-  getActiveTab().then(showCookiesForTab);
+
+  function showOptions(currentTabs) {
+    console.log('in options')
+    tabs = currentTabs;
+    setHeaders(VIEWS.main)
+    populateAgents()
+  }
+
+  getActiveTab().then(showOptions);
