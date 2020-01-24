01ad3e77714c3ed16fbb00d783406c052e5b2795
obit91 <ohabel@gmail.com>
2020-01-24 19:28:33 +0200

    Reverted back to browser action, now retrieves data based on tab

--- src/Observer.js
+++ src/Observer.js
@@ -1,30 +1,39 @@
-const getCookies = () => {
-    const cookies = {};
-    allCookies
-        .split(';')
-        .map(cookieItem => cookieItem
-            .split("="))
-        .forEach(item => cookies[item[0]] = cookies[1]);
-    return cookies;
-}
-
-handleClick = (e) => {
-
-    const chosenValue = e.target.textContent;
-
-    console.log(`Chose: ${chosenValue}.`)
-
-    switch (chosenValue) {
-        case ('cookies'):
-            console.log('Clicked cookies.')
-            const cookies = getCookies();
-            console.log(cookies);
-            break;
-        default:
-            break;
-    }
-};
-
-browser.pageAction.onClicked.addListener(handleClick);
-
-console.log('Exiting observer.')
+function showCookiesForTab(tabs) {
+    //get the first tab object in the array
+    let tab = tabs.pop();
+  
+    //get all cookies in the domain
+    var gettingAllCookies = browser.cookies.getAll({url: tab.url});
+    gettingAllCookies.then((cookies) => {
+  
+      //set the header of the panel
+      var activeTabUrl = document.getElementById('header-title');
+      var text = document.createTextNode("Cookies at: "+tab.title);
+      var cookieList = document.getElementById('cookie-list');
+      activeTabUrl.appendChild(text);
+  
+      if (cookies.length > 0) {
+        //add an <li> item with the name and value of the cookie to the list
+        for (let cookie of cookies) {
+          let li = document.createElement("li");
+          let content = document.createTextNode(cookie.name + ": "+ cookie.value);
+          li.appendChild(content);
+          cookieList.appendChild(li);
+        }
+      } else {
+        let p = document.createElement("p");
+        let content = document.createTextNode("No cookies in this tab.");
+        let parent = cookieList.parentNode;
+  
+        p.appendChild(content);
+        parent.appendChild(p);
+      }
+    });
+  }
+  
+  //get active tab to run an callback function.
+  //it sends to our callback an array of tab objects
+  function getActiveTab() {
+    return browser.tabs.query({currentWindow: true, active: true});
+  }
+  getActiveTab().then(showCookiesForTab);
