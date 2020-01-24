3fc211a21bce009e03e777645c0e6202457ec45f
obit91 <ohabel@gmail.com>
2020-01-24 18:50:29 +0200

    Converted the popup to a Page Action instead of a Browser Action.

--- src/Observer.js
+++ src/Observer.js
@@ -1,15 +1,30 @@
-browser.browserAction.onClicked.addListener((tab) => {
-    console.log('Clicked extension.')
-    const cookies = getCookies();
-    console.log(cookies);
-});
-
-getCookies = () => {
-    const cookies = {}
+const getCookies = () => {
+    const cookies = {};
     allCookies
         .split(';')
         .map(cookieItem => cookieItem
             .split("="))
-        .forEach(item => cookies[item[0]] = cookies[1])
+        .forEach(item => cookies[item[0]] = cookies[1]);
     return cookies;
 }
+
+handleClick = (e) => {
+
+    const chosenValue = e.target.textContent;
+
+    console.log(`Chose: ${chosenValue}.`)
+
+    switch (chosenValue) {
+        case ('cookies'):
+            console.log('Clicked cookies.')
+            const cookies = getCookies();
+            console.log(cookies);
+            break;
+        default:
+            break;
+    }
+};
+
+browser.pageAction.onClicked.addListener(handleClick);
+
+console.log('Exiting observer.')
